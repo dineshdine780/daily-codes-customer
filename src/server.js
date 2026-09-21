c603a124d2b2addbe987b909d61d27462f4267fb
dineshdine780 <dineshdine8373@gmail.com>
2026-09-21 17:30:54 +0530

    updated all files

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-
 const authRoutes = require("./routes/authRoutes");
 const dailyCodeRoutes = require("./routes/dailyCodeRoutes");
 const savedCodeRoutes = require("./routes/savedCodeRoutes");
@@ -16,23 +15,26 @@ const categoryRoutes = require("./routes/categoryRoutes");
 
 connectDB();
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  process.env.FRONTEND_URL,
+  process.env.ADMIN_FRONTEND_URL,
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-    ],
+    origin: allowedOrigins,
   })
 );
 
 app.use(express.json());
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/daily-codes", dailyCodeRoutes);
 app.use("/api/saved-codes", savedCodeRoutes);
 app.use("/api/liked-codes", likedCodeRoutes);
-app.use("/api/admin/dashboard",adminDashboardRoutes);
+app.use("/api/admin/dashboard", adminDashboardRoutes);
 app.use("/api/categories", categoryRoutes);
 
 app.get("/", (req, res) => {
@@ -41,8 +43,6 @@ app.get("/", (req, res) => {
     message: "Daily Codes Backend Running",
   });
 });
-
-
 
 const PORT = process.env.PORT || 5000;
 
